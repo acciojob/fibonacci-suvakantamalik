8184213cd9e65b8176f438c06a92f38da773f8e4
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-08-12 18:48:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
 // your code here
+	let n1 = 0;
+	let n2 = 1;
+	let sum;
+	if(num === 1){
+		return n1;
+	}
+	else if(num === 2){
+		return n2;
+	}
+	else{
+		for(let i=3; i<=num; i++){
+			sum = n1+n2;
+			n1 = n2;
+			n2 = sum;
+		}
+		return n2;
+	}
 }
 
 module.exports = fibonacci;
